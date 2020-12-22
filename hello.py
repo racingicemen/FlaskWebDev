45eb3a359e7c1524afab7107551f142315d954ae
Racing Icemen <rkkommu@ucdavis.edu>
2020-12-22 11:37:05 -0800

    Completed upto 3a

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return f"<h1>Hello, {name}!</h1>"
+    return render_template('user.html', name=name)
