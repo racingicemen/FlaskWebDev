8482a6ea4ca95589b6595dfdbf76be2b25f5e6a1
Racing Icemen (orange) <rkkommu@ucdavis.edu>
2021-01-13 16:58:37 -0800

    Completed upto 11b

--- app/main/views.py
+++ app/main/views.py
@@ -22,7 +22,10 @@ def index():
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    if user is None:
+        abort(404)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
 
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
